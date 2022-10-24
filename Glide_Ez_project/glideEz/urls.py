ccbfafad0ea54543fa8e01275f03b7200bf5634f
Akshat202002 <akshatsamdani23@gmail.com>
2022-10-24 15:18:20 +0530

    updated paths in index.html

--- Glide_Ez_project/glideEz/urls.py
+++ Glide_Ez_project/glideEz/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("destination", views.destination_view, name="destination"),
+    path("login_user", views.login_user_view, name="login_user"),
+    path("login_airline", views.login_airline_view, name="login_airline"),
+    path("pricing", views.pricing_view, name="pricing")
     
 ]
--- Glide_Ez_project/glideEz/views.py
+++ Glide_Ez_project/glideEz/views.py
@@ -5,4 +5,15 @@ from django.http import HttpResponse
 def home(request): 
     return render(request, "glideEz/index.html")
 
+def destination_view(request):
+    return render(request, "glideEz/destination.html")
+
+def login_user_view(request):
+    return render(request, "glideEz/login_user.html")
+
+def login_airline_view(request):
+    return render(request, "glideEz/login_airline.html")
+
+def pricing_view(request):
+    return render(request, "glideEz/pricing.html")
     
