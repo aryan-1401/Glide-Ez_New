fdb1b2f2b3e129b5f2cb8a8b7c7a0bf8030a73ef
Akshat202002 <akshatsamdani23@gmail.com>
2022-10-25 13:02:37 +0530

    added backend for contact us

--- Glide_Ez_project/Glide_Ez_project/settings.py
+++ Glide_Ez_project/Glide_Ez_project/settings.py
@@ -135,3 +135,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# EMAIL CONFIGURATION (for contact form)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'glideezinfo@gmail.com'
+EMAIL_HOST_PASSWORD = 'adruabczlgqvvvzy'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
--- Glide_Ez_project/glideEz/views.py
+++ Glide_Ez_project/glideEz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import HttpResponse, render
 from django.http import HttpResponse
+from django.core.mail import send_mail
 # Create your views here.
 
 def home(request): 
@@ -18,5 +19,24 @@ def pricing_view(request):
     return render(request, "glideEz/pricing.html")
 
 def contact_view(request):
+    # TODO: fix no message in email
+    if request.method == 'POST':
+        name = request.POST.get('visitor_name')
+        email = request.POST.get('visitor_email')
+        subject = request.POST.get('email_title')
+        message = request.POST.get('visitor_message')
+        form_data = {
+            'name':name,
+            'email':email,
+            'subject':subject,
+            'message':message
+        }
+        message = '''
+        From:\n\t\t{}\n
+        Message:\n\t\t{}\n
+        Email:\n\t\t{}\n
+        Subject:\n\t\t{}\n
+        '''.format(form_data['name'], form_data['message'], form_data['email'],form_data['subject'])
+        send_mail('You got a mail!', message, '', ['glideezinfo@gmail.com']) # TODO: enter your email address
     return render(request, "glideEz/contact.html")
     
