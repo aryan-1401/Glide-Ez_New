b13516a45c451036fd7536e9030bb01017e81e2c
Akshat202002 <akshatsamdani23@gmail.com>
2022-11-03 01:01:35 +0530

    added login validation for book flight

--- Glide_Ez_project/glideEz/views.py
+++ Glide_Ez_project/glideEz/views.py
@@ -381,7 +381,9 @@ def search_flight_view(request):
         return render(request, "glideEz/search_flight.html", {'details': details , 'source' : source , 'destination' : destination , 'Class_Type' : class_type})
 
 def book_flight_view(request):
-
+    # Check is user is logged in 
+    if 'email' not in request.session:
+        return redirect('/login_user')
     return render(request, "glideEz/book_flight.html")
 
 def airline_home_view(request):
