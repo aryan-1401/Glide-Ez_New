c16b86121fed260da471e27282e5e0f4c7260dd4
Akshat202002 <akshatsamdani23@gmail.com>
2022-11-05 18:04:58 +0530

    added validation for register user

--- Glide_Ez_project/glideEz/static/glideEz/js/all_validate.js
+++ Glide_Ez_project/glideEz/static/glideEz/js/all_validate.js
@@ -1,3 +1,5 @@
+// For index
+
 function searchvalidate() {
   let src = document.forms["searchtrip"]["source"].value;
   let dest = document.forms["searchtrip"]["destination"].value;
@@ -29,6 +31,66 @@ function searchvalidate() {
   }
   if (travel < today) {
     alert("INVALID DATE");
+    return false;
+  }
+}
+
+// For register
+
+function registervalidate() {
+  let name = document.forms["register"]["username"].value;
+  let email = document.forms["register"]["email"].value;
+  let phone = document.forms["register"]["phone"].value;
+  let password = document.forms["register"]["pass"].value;
+
+  let aadhar = document.forms["register"]["aadhar"].value;
+  let dob = document.forms["register"]["dob"].value;
+  let address = document.forms["register"]["address"].value;
+
+  if (name == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  if (email == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+  if (phone == "") {
+    alert("Phone must be filled out");
+    return false;
+  }
+  if (password == "") {
+    alert("Password must be filled out");
+    return false;
+  }
+  if (aadhar == "") {
+    alert("Aadhar must be filled out");
+    return false;
+  }
+  if (dob == "") {
+    alert("Date of Birth must be filled out");
+    return false;
+  }
+  if (address == "") {
+    alert("Address must be filled out");
+    return false;
+  }
+  if (phone.length != 10) {
+    alert("Phone number must be 10 digits");
+    return false;
+  }
+  if (aadhar.length != 12) {
+    alert("Aadhar number must be 12 digits");
+    return false;
+  }
+  // Check if password is of length 8
+  if (password.length < 8) {
+    alert("Password must be of length 8");
+    return false;
+  }
+  // Check for valid email
+  if (email.indexOf("@") == -1 || email.indexOf(".") == -1) {
+    alert("Invalid Email");
     return false;
   }
 }
